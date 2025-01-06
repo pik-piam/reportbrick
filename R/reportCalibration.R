9833b0c7c57920beab542384708769182d2697fd
Ricarda Rosemann <ricarda.rosemann@pik-potsdam.de>
2025-01-06 10:44:17 +0100

    Adapt calibration reporting to new calibration implementation

--- R/reportCalibration.R
+++ R/reportCalibration.R
@@ -7,7 +7,8 @@
 #' @author Ricarda Rosemann
 #'
 #' @importFrom tidyr crossing replace_na
-#' @importFrom utils write.csv
+#' @importFrom utils read.csv write.csv
+#' @importFrom yaml read_yaml
 #' @export
 
 reportCalibration <- function(gdx) {
@@ -18,69 +19,89 @@ reportCalibration <- function(gdx) {
   path <- dirname(gdx)
 
   allFiles <- list.files(path = path,
-                         pattern = paste0(sub("\\.gdx$", "", basename(gdx)),
+                         pattern = paste0(sub("_0\\.gdx$", "", basename(gdx)),
                                           "_\\d{1,3}\\.gdx"))
   maxIter <- max(as.numeric(sub(".*_(\\d{1,3})\\.gdx$", "\\1", allFiles)))
 
+  gdxInp <- file.path(path, "input.gdx")
+
+  # Read config
+  cfg <- read_yaml(file = file.path(path, "config", "config_COMPILED.yaml"))
+
   # Read relevant time periods
-  tCalib <- readGdxSymbol(gdx, "tCalib", asMagpie = FALSE)[["ttot"]]
+  tCalib <- cfg[["calibperiods"]]
 
-  # Read different parameters from gdx files for all iterations
-  # TODO: Could potentially replace this call by mip::getPlotData (but that uses gdxrrw and can only start counting at 1) # nolint
-  targetFunction <- .readGdxIter(gdx, "p_f", maxIter, asMagpie = FALSE)
+  calibOptim <- cfg[["switches"]][["RUNTYPE"]] == "calibrationOptimization"
 
-  stepSize <- .readGdxIter(gdx, "p_alpha", maxIter, asMagpie = FALSE)
+  # Read diagnostic parameters from csv
+  stepSize <- read.csv(file.path(path, "stepSizeParamsIter.csv")) %>%
+    select(-"delta", -"phiDeriv") %>%
+    rename(value = "stepSize")
+  descDirCon <- read.csv(file.path(path, "deviationConIter.csv")) %>%
+    rename(value = "d") %>%
+    .replaceVarName()
+  descDirRen <- read.csv(file.path(path, "deviationRenIter.csv")) %>%
+    rename(value = "d")
 
   # Potentially shift the time filter to a later stage if I want to save and plot pure stock/flow data
-  p_stock <- .readGdxIter(gdx, "p_stock", maxIter, asMagpie = FALSE, ttotFilter = tCalib, replaceVar = TRUE)
+  v_stock <- .readGdxIter(gdx,
+                          if (calibOptim) "p_stock" else "v_stock",
+                          maxIter, asMagpie = FALSE, ttotFilter = tCalib, replaceVar = TRUE)
 
-  p_construction <- .readGdxIter(gdx, "p_construction", maxIter,
+  v_construction <- .readGdxIter(gdx,
+                                 if (calibOptim) "p_construction" else "v_construction",
+                                 maxIter,
                                  asMagpie = FALSE, ttotFilter = tCalib, replaceVar = TRUE)
 
-  p_renovation <- .readGdxIter(gdx, "p_renovation", maxIter,
+  v_renovation <- .readGdxIter(gdx,
+                               if (calibOptim) "p_renovation" else "v_renovation", maxIter,
                                asMagpie = FALSE, ttotFilter = tCalib)
 
-  # Read historical gdx files
-  p_stockHist <- .replaceVarName(readGdxSymbol(gdx, "p_stockHist", asMagpie = FALSE))
+  # Read input gdx file
+  p_stockCalibTarget <- .replaceVarName(readGdxSymbol(gdxInp,
+                                                      if (calibOptim) "p_stockCalibTarget" else "p_stockHist",
+                                                      asMagpie = FALSE))
 
-  p_constructionHist <- .replaceVarName(readGdxSymbol(gdx, "p_constructionHist", asMagpie = FALSE))
+  p_constructionCalibTarget <- .replaceVarName(readGdxSymbol(gdxInp, "p_constructionCalibTarget", asMagpie = FALSE))
 
-  p_renovationHist <- readGdxSymbol(gdx, "p_renovationHist", asMagpie = FALSE)
+  p_renovationCalibTarget <- readGdxSymbol(gdxInp, "p_renovationCalibTarget", asMagpie = FALSE)
 
 
   # COMPUTE DEVIATIONS ---------------------------------------------------------
 
-  p_stockDev <- .computeDeviation(p_stock, p_stockHist)
+  v_stockDev <- .computeDeviation(v_stock, p_stockCalibTarget)
 
-  p_constructionDev <- .computeDeviation(p_construction, p_constructionHist)
+  v_constructionDev <- .computeDeviation(v_construction, p_constructionCalibTarget)
 
-  p_renovationDev <- .computeDeviation(p_renovation, p_renovationHist)
+  v_renovationDev <- .computeDeviation(v_renovation, p_renovationCalibTarget)
 
-  # Start writing output quantities --------------------------------------------
+  # WRITE DIAGNOSTIC OUTPUT ----------------------------------------------------
 
   out <- list()
 
-  out[["targetFunction"]] <- targetFunction
-
   out[["stepSize"]] <- stepSize
+
+  out[["descDirCon"]] <- .computeAvg(descDirCon, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr", "ttot"))
+
+  out[["descDirRen"]] <- .computeAvg(descDirRen, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr", "ttot"))
 
   # AGGREGATE QUANTITIES -------------------------------------------------------
 
   # Aggregate across all dimensions
-  out[["stockDevAgg"]] <- .computeSumSq(p_stockDev, rprt = c("iteration", "reg", "typ", "loc", "inc"))
+  out[["stockDevAgg"]] <- .computeSumSq(v_stockDev, rprt = c("iteration", "reg", "typ", "loc", "inc"))
 
-  out[["conDevAgg"]] <- .computeSumSq(p_constructionDev, rprt = c("iteration", "reg", "typ", "loc", "inc"))
+  out[["conDevAgg"]] <- .computeSumSq(v_constructionDev, rprt = c("iteration", "reg", "typ", "loc", "inc"))
 
-  out[["renDevAgg"]] <- .computeSumSq(p_renovationDev, rprt = c("iteration", "reg", "typ", "loc", "inc"))
+  out[["renDevAgg"]] <- .computeSumSq(v_renovationDev, rprt = c("iteration", "reg", "typ", "loc", "inc"))
 
   out[["flowDevAgg"]] <- .computeFlowSum(out[["conDevAgg"]], out[["renDevAgg"]])
 
   # Aggregate by heating system (hs)
-  out[["stockDevHs"]] <- .computeSumSq(p_stockDev, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr"))
+  out[["stockDevHs"]] <- .computeSumSq(v_stockDev, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr"))
 
-  out[["conDevHs"]] <- .computeSumSq(p_constructionDev, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr"))
+  out[["conDevHs"]] <- .computeSumSq(v_constructionDev, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr"))
 
-  out[["renDevHs"]] <- .computeSumSq(p_renovationDev, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr"))
+  out[["renDevHs"]] <- .computeSumSq(v_renovationDev, rprt = c("iteration", "reg", "typ", "loc", "inc", "hsr"))
 
   out[["flowDevHs"]] <- .computeFlowSum(out[["conDevHs"]], out[["renDevHs"]])
 
@@ -88,23 +109,25 @@ reportCalibration <- function(gdx) {
   # COMPUTE RELATIVE AGGREGATE QUANTITIES --------------------------------------
 
   # Across all dimensions
-  out[["stockDevRel"]] <- .computeRelDev(out[["stockDevAgg"]], p_stockHist, tCalib)
+  out[["stockDevRel"]] <- .computeRelDev(out[["stockDevAgg"]], p_stockCalibTarget, tCalib)
 
-  out[["conDevRel"]] <- .computeRelDev(out[["conDevAgg"]], p_constructionHist, tCalib)
+  out[["conDevRel"]] <- .computeRelDev(out[["conDevAgg"]], p_constructionCalibTarget, tCalib)
 
-  out[["renDevRel"]] <- .computeRelDev(out[["renDevAgg"]], p_renovationHist, tCalib)
+  out[["renDevRel"]] <- .computeRelDev(out[["renDevAgg"]], p_renovationCalibTarget, tCalib)
 
-  out[["flowDevRel"]] <- .computeRelDev(out[["flowDevAgg"]], list(p_constructionHist, p_renovationHist), tCalib)
+  out[["flowDevRel"]] <- .computeRelDev(out[["flowDevAgg"]], list(p_constructionCalibTarget, p_renovationCalibTarget),
+                                        tCalib)
 
   # Separately for all heating systems (hs)
-  out[["stockDevHsRel"]] <- .computeRelDev(out[["stockDevHs"]], p_stockHist, tCalib, notInHist = "hsr")
+  out[["stockDevHsRel"]] <- .computeRelDev(out[["stockDevHs"]], p_stockCalibTarget, tCalib, notInTargetGrp = "hsr")
 
-  out[["conDevHsRel"]] <- .computeRelDev(out[["conDevHs"]], p_constructionHist, tCalib, notInHist = "hsr")
+  out[["conDevHsRel"]] <- .computeRelDev(out[["conDevHs"]], p_constructionCalibTarget, tCalib, notInTargetGrp = "hsr")
 
-  out[["renDevHsRel"]] <- .computeRelDev(out[["renDevHs"]], p_renovationHist, tCalib, notInHist = "hsr")
+  out[["renDevHsRel"]] <- .computeRelDev(out[["renDevHs"]], p_renovationCalibTarget, tCalib, notInTargetGrp = "hsr")
 
-  out[["flowDevHsRel"]] <- .computeRelDev(out[["flowDevHs"]], list(p_constructionHist, p_renovationHist),
-                                          tCalib, notInHist = "hsr")
+  out[["flowDevHsRel"]] <- .computeRelDev(out[["flowDevHs"]], list(p_constructionCalibTarget, p_renovationCalibTarget),
+                                          tCalib, notInTargetGrp = "hsr")
+
 
   # EXPAND DIMENSIONS AND COMBINE IN ONE DATA FRAME ----------------------------
 
@@ -141,7 +164,7 @@ reportCalibration <- function(gdx) {
   # Loop over all iterations and read in gdx files
   res <- data.frame()
   for (i in seq(0, maxIter)) {
-    fileName <- file.path(dirname(gdx), paste0(gsub("\\.gdx$", "", basename(gdx)), "_", i, ".gdx"))
+    fileName <- file.path(dirname(gdx), paste0(gsub("_0\\.gdx$", "", basename(gdx)), "_", i, ".gdx"))
     if (file.exists(fileName)) {
       res <- rbind(res, readGdxSymbol(fileName, symbol, asMagpie = asMagpie) %>%
                      mutate(iteration = i))
@@ -182,30 +205,46 @@ reportCalibration <- function(gdx) {
   return(df)
 }
 
-#' Compute the deviation to historical data
+#' Compute the deviation to target data
 #'
 #' @param df data frame, containing data from calibration run
-#' @param dfHist data frame, containing historical data
-#' @returns data frame where the value column contains the deviation from historical data
+#' @param dfTarget data frame, containing historical data as calibration target
+#' @returns data frame where the value column contains the deviation from target data
 #'
 #' @importFrom dplyr %>% .data filter full_join mutate rename select
 #' @importFrom tidyr crossing replace_na
 
-.computeDeviation <- function(df, dfHist) {
+.computeDeviation <- function(df, dfTarget) {
 
   # Add iteration column to historical data and filter for time periods of calibration data in df
-  dfHist <- dfHist %>%
+  dfTarget <- dfTarget %>%
     crossing(iteration = df[["iteration"]]) %>%
     filter(.data[["ttot"]] %in% unique(df[["ttot"]])) %>%
-    rename(hist = "value")
+    rename(target = "value")
 
   # Replace all data points not present in either data frames by zero and compute the difference
   df <- df %>%
-    full_join(dfHist,
-              by = intersect(colnames(df), colnames(dfHist))) %>% # Rather unsure about this ...
-    replace_na(list(value = 0, hist = 0)) %>%
-    mutate(value = .data[["value"]] - .data[["hist"]]) %>%
-    select(-"hist")
+    full_join(dfTarget,
+              by = intersect(colnames(df), colnames(dfTarget))) %>% # Rather unsure about this ...
+    replace_na(list(value = 0, target = 0)) %>%
+    mutate(value = .data[["value"]] - .data[["target"]]) %>%
+    select(-"target")
+
+}
+
+#' Compute the sum of the squares in a data frame
+#'
+#' @param df data frame, containing the data to be evaluated
+#' @param rprt character, column names for which the sum of the squares should be reported separately
+#' @returns data frame averages as value column
+#'
+#' @importFrom dplyr %>% across any_of .data group_by summarise
+#'
+.computeAvg <- function(df, rprt = "") {
+
+  df %>%
+    group_by(across(any_of(rprt))) %>%
+    summarise(value = mean(.data[["value"]], na.rm = TRUE), .groups = "drop")
 
 }
 
@@ -225,46 +264,46 @@ reportCalibration <- function(gdx) {
 
 }
 
-#' Compute the relative deviation to historical data as the relative euclidean distance
+#' Compute the relative deviation to calibration target data as the relative euclidean distance
 #'
-#' @param dfDev data frame, containing deviation between calibration and historical data
-#' @param dfHist data frame or a list of one or two data frames, containing historical data.
+#' @param dfDev data frame, containing deviation between calibration result and target data
+#' @param dfTarget data frame or a list of one or two data frames, containing target data.
 #'  The option to pass two data frames is used to combine construction and renovation flows.
 #'  If two data frames are passed, the first one is assumed to be construction, the second renovation
-#' @param tCalib numerical/factor, calibration time periods to filter historical data
-#' @param notInHist character vector, pass columns that historical data should not be grouped by
-#'  when computing the sum of the squares
+#' @param tCalib numerical/factor, calibration time periods to filter calibration target data
+#' @param notInTargetGrp character vector, pass columns that target data should not be grouped by
+#'  when computing the sum of the squares, although they exist in the data
 #' @returns data frame with relative deviation in value column
 #'
 #' @importFrom dplyr %>% .data filter left_join mutate rename select
 
-.computeRelDev <- function(dfDev, dfHist, tCalib, notInHist = NULL) {
+.computeRelDev <- function(dfDev, dfTarget, tCalib, notInTargetGrp = NULL) {
 
   # Determine the reported columns in the calibration data
-  rprt <- setdiff(colnames(dfDev), c("iteration", "value", notInHist))
+  rprt <- setdiff(colnames(dfDev), c("iteration", "value", notInTargetGrp))
 
-  # Convert historical data to list if necessary
-  if (is.data.frame(dfHist)) dfHist <- list(dfHist)
+  # Convert target data to list if necessary
+  if (is.data.frame(dfTarget)) dfTarget <- list(dfTarget)
 
-  # Compute the sum of the squares for historical data
-  dfHistSumList <- lapply(dfHist, function(df) {
+  # Compute the sum of the squares for target data
+  dfTargetSumList <- lapply(dfTarget, function(df) {
     df %>%
       filter(.data[["ttot"]] %in% tCalib) %>%
       .computeSumSq(rprt = rprt)
   })
 
-  if (length(dfHistSumList) == 1) {
-    dfHistSum <- dfHistSumList[[1]] %>%
-      rename(hist = "value")
-  } else if (length(dfHistSumList) >= 2) {
+  if (length(dfTargetSumList) == 1) {
+    dfTargetSum <- dfTargetSumList[[1]] %>%
+      rename(target = "value")
+  } else if (length(dfTargetSumList) >= 2) {
 
-    # If two data frames with historical data have been passed:
+    # If two data frames with target data have been passed:
     # Combine them by treating the first as construction flows and the second as renovation flows
-    dfHistSum <- .computeFlowSum(dfHistSumList[[1]], dfHistSumList[[2]]) %>%
-      rename(hist = "value")
+    dfTargetSum <- .computeFlowSum(dfTargetSumList[[1]], dfTargetSumList[[2]]) %>%
+      rename(target = "value")
   }
 
-  if (length(dfHistSumList) > 2) {
+  if (length(dfTargetSumList) > 2) {
     warning(paste("Only two data frames of historical data can be combined.",
                   "The remaining data frames are ignored."))
   }
@@ -272,9 +311,9 @@ reportCalibration <- function(gdx) {
   # Compute the relative deviation as the square root of the deviation divided
   # by the square root of the historical data
   dfDev <- dfDev %>%
-    left_join(dfHistSum, by = rprt) %>%
-    mutate(value = sqrt(.data[["value"]]) / sqrt(.data[["hist"]])) %>%
-    select(-"hist")
+    left_join(dfTargetSum, by = rprt) %>%
+    mutate(value = sqrt(.data[["value"]]) / sqrt(.data[["target"]])) %>%
+    select(-"target")
 }
 
 #' Compute the sum of construction and renovation flow values
